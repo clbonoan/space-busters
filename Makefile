dc10f2d8657b6dcf893f04e5ee68da85c0c7cdbd
ChristineLeanna Bonoan <cbonoan2@odin.cs.csubak.edu>
2025-03-11 07:45:13 -0700

    fixed Makefile, moved functionality for images to space.cpp

--- Makefile
+++ Makefile
@@ -2,10 +2,7 @@ CFLAGS = -I ./include
 ##LIB 	= ./libggfonts.so
 LFLAGS = -lrt -lX11 -lGLU -lGL -pthread -lm #-lXrander
 
-all: asteroids space
-
-asteroids: asteroids.cpp log.cpp timers.cpp cbonoan2.cpp eaviles.cpp davalos.cpp bbarrios.cpp mgarris.cpp
-	g++ $(CFLAGS) asteroids.cpp cbonoan2.cpp eaviles.cpp davalos.cpp bbarrios.cpp mgarris.cpp log.cpp timers.cpp libggfonts.a -Wall -Wextra $(LFLAGS) -o asteroids
+all: space
 
 space: space.cpp log.cpp timers.cpp cbonoan2.cpp eaviles.cpp davalos.cpp bbarrios.cpp mgarris.cpp
 	g++ $(CFLAGS) space.cpp cbonoan2.cpp eaviles.cpp davalos.cpp bbarrios.cpp mgarris.cpp log.cpp timers.cpp libggfonts.a -Wall -Wextra $(LFLAGS) -o space
--- space.cpp
+++ space.cpp
@@ -240,6 +240,65 @@ public:
     }
 } g;
 
+class Image {
+public:
+    int width, height;
+    unsigned char *data;
+    ~Image() { delete [] data; }
+    Image(const char *fname) {
+        if (fname[0] == '\0')
+            return;
+        //printf("fname **%s**\n", fname);
+        int ppmFlag = 0;
+        char name[40];
+        strcpy(name, fname);
+        int slen = strlen(name);
+        char ppmname[80];
+        if (strncmp(name+(slen-4), ".ppm", 4) == 0)
+            ppmFlag = 1;
+        if (ppmFlag) {
+            strcpy(ppmname, name);
+        } else {
+            name[slen-4] = '\0';
+            //printf("name **%s**\n", name);
+            sprintf(ppmname,"%s.ppm", name);
+            //printf("ppmname **%s**\n", ppmname);
+            char ts[100];
+            //system("convert eball.jpg eball.ppm");
+            sprintf(ts, "convert %s %s", fname, ppmname);
+            system(ts);
+        }   
+        //sprintf(ts, "%s", name);
+        FILE *fpi = fopen(ppmname, "r");
+        if (fpi) {
+            char line[200];
+            fgets(line, 200, fpi);
+            fgets(line, 200, fpi);
+            //skip comments and blank lines
+            while (line[0] == '#' || strlen(line) < 2)
+                fgets(line, 200, fpi);
+            sscanf(line, "%i %i", &width, &height);
+            fgets(line, 200, fpi);
+            //get pixel data
+            int n = width * height * 3;
+            data = new unsigned char[n];
+            for (int i=0; i<n; i++)
+                data[i] = fgetc(fpi);
+            fclose(fpi);
+        } else {
+            printf("ERROR opening image: %s\n",ppmname);
+            exit(0);
+        }   
+        if (!ppmFlag)
+            unlink(ppmname);
+    }   
+};
+Image img[2] = { 
+"./images/stardust-health.png",
+"./images/ufo.png"
+};
+
+
 //X Windows variables
 class X11_wrapper {
 private:
@@ -423,6 +482,49 @@ int main()
 	return 0;
 }
 
+unsigned char *buildAlphaData(Image *img)
+{
+    //Add 4th component to an RGB stream...
+    //RGBA
+    //When you do this, OpenGL is able to use the A component to determine
+    //transparency information.
+    //It is used in this application to erase parts of a texture-map from view.
+    int i;
+    int a,b,c;
+    unsigned char *newdata, *ptr;
+    unsigned char *data = (unsigned char *)img->data;
+    newdata = (unsigned char *)malloc(img->width * img->height * 4);
+    ptr = newdata;
+    for (i=0; i<img->width * img->height * 3; i+=3) {
+        a = *(data+0);
+        b = *(data+1);
+        c = *(data+2);
+        *(ptr+0) = a;
+        *(ptr+1) = b;
+        *(ptr+2) = c;
+        //-----------------------------------------------
+        //get largest color component...
+        //*(ptr+3) = (unsigned char)((
+        //      (int)*(ptr+0) +
+        //      (int)*(ptr+1) +
+        //      (int)*(ptr+2)) / 3);
+        //d = a;
+        //if (b >= a && b >= c) d = b;
+        //if (c >= a && c >= b) d = c;
+        //*(ptr+3) = d;
+        //-----------------------------------------------
+        //this code optimizes the commented code above.
+        //code contributed by student: Chris Smith
+        //
+        *(ptr+3) = (a|b|c);
+        //-----------------------------------------------
+        ptr += 4;
+        data += 3;
+    }
+    return newdata;
+}
+
+
 void init_opengl(void)
 {
 	//OpenGL initialization
@@ -443,8 +545,6 @@ void init_opengl(void)
 	//Do this to allow fonts
 	glEnable(GL_TEXTURE_2D);
 	initialize_fonts();
-    //do this to allow ufo texture
-    initTextures();
 }
 
 
